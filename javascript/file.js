9cdf453fd530ad8d87592550ea70943b58da464f
irinaoloinic <133090430+irinaoloinic@users.noreply.github.com>
2023-08-10 08:18:46 +0200

    added the set interval to update the hour in milliseconds

--- javascript/file.js
+++ javascript/file.js
@@ -1,11 +1,13 @@
+function updateTime(){
 //Barcelona//
 let barcelonaElement = document.querySelector("#barcelona");
 let barcelonaDateElement = barcelonaElement.querySelector(".date");
 let barcelonaTimeElement = barcelonaElement.querySelector(".time");
 let barcelonaTime = moment().tz("Europe/Madrid");
 
-barcelonaDateElement.innerHTML = moment().format("MMMM Do YYYY");
-barcelonaTimeElement.innerHTML = barcelonaTime.format("h:mm:ss [<small>]A[<small>]");
+barcelonaDateElement.innerHTML = barcelonaTime.format("MMMM Do YYYY");
+barcelonaTimeElement.innerHTML = barcelonaTime.format("h:mm:ss SSS[<small>]A[<small>]");
+
 
 //Sydney//
 let sydneyElement = document.querySelector("#sydney");
@@ -13,6 +15,9 @@ let sydneyDateElement = sydneyElement.querySelector(".date");
 let sydneyTimeElement = sydneyElement.querySelector(".time");
 let sydneyTime = moment().tz("Australia/Sydney");
 
-sydneyDateElement.innerHTML = moment().format("MMMM Do YYYY");
-sydneyTimeElement.innerHTML = sydneyTime.format("h:mm:ss [<small>]A[<small>]");
+sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do YYYY");
+sydneyTimeElement.innerHTML = sydneyTime.format("h:mm:ss SSS[<small>]A[<small>]");
+}
 
+updateTime()
+setInterval(updateTime, 100);
